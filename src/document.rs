a0a5231c6d1346523026826028ae67047c627df5
Anurag Chemban <chembananurag23@gmail.com>
2023-08-11 17:16:34 +0530

    editor: insert and delete char --start

--- src/document.rs
+++ src/document.rs
@@ -1,3 +1,4 @@
+use crate::CursorPosition;
 use crate::Row;
 use std::fs;
 
@@ -32,5 +33,29 @@ impl Document {
 
     pub fn length(&self) -> usize {
         self.rows.len()
+    }
+
+    pub fn insert(&mut self, at_posi: &CursorPosition, character: char) {
+        if at_posi.y == self.length() {
+            let mut row = Row::default();
+            row.insert(0, character);
+            self.rows.push(row);
+        } else if at_posi.y < self.length() {
+            let row = self.rows.get_mut(at_posi.y).unwrap();
+            row.insert(at_posi.x, character);
+        }
+    }
+
+    pub fn delete(&mut self, at_posi: &CursorPosition) {
+        if at_posi.y >= self.length() {
+            return;
+        } else {
+            let row = self.rows.get_mut(at_posi.y).unwrap();
+            row.delete(at_posi.x);
+        }
+    }
+
+    pub fn append(&mut self, line: Row) {
+        
     }
 }
--- src/row.rs
+++ src/row.rs
@@ -1,6 +1,7 @@
 use std::cmp;
 use unicode_segmentation::UnicodeSegmentation;
 
+#[derive(Default)]
 pub struct Row {
     string: String,
     len: usize,
@@ -51,5 +52,36 @@ impl Row {
 
     pub fn is_empty(&self) -> bool {
         self.len == 0
+    }
+
+    pub fn insert(&mut self, at: usize, character: char) {
+        if at >= self.length() {
+            self.string.push(character);
+        } else {
+            let mut result: String = self.string[..].graphemes(true).take(at).collect();
+            let second_half: String = self.string[..].graphemes(true).skip(at).collect();
+
+            result.push(character);
+            result.push_str(&second_half);
+
+            self.string = result;
+        }
+
+        self.update_len();
+    }
+
+    pub fn delete(&mut self, at: usize) {
+        if at >= self.length() {
+            return;
+        } else {
+            let mut first_half: String = self.string[..].graphemes(true).take(at).collect();
+            let second_half: String = self.string[..].graphemes(true).skip(at + 1).collect();
+
+            first_half.push_str(&second_half);
+
+            self.string = first_half;
+        }
+
+        self.update_len();
     }
 }
